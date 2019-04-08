3177f41bb2c3f7af1e6868fa362d0990a05b47aa
Pavan Kumar C <pavankumar@railsfactory.org>
2019-04-08 15:38:28 +0530

    firebase library added for Authentication

--- src/App.js
+++ src/App.js
@@ -3,8 +3,22 @@ import { SafeAreaView, Text } from 'react-native'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import reducers from './reducers'
+import firebase from 'firebase'
 
 class App extends Component {
+
+  componentWillMount() {
+    const config = {
+      apiKey: "AIzaSyCZcTv0P2HSG6Hv28wLl1aX4FyW3Tat5Gc",
+      authDomain: "empolyeemanager.firebaseapp.com",
+      databaseURL: "https://empolyeemanager.firebaseio.com",
+      projectId: "empolyeemanager",
+      storageBucket: "empolyeemanager.appspot.com",
+      messagingSenderId: "1083124988637"
+    }
+    firebase.initializeApp(config)
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
